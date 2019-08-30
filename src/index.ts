eab7aea412ec971b946080fe7b1016c23c0f5f19
niiuchi <niiuchi@fork.co.jp>
2019-08-30 18:32:08 +0900

    tenpary commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
 import axios from 'axios'
 
-const fetchGoogleBookApi: any = () => {
+const resultLists: any[] = []
+let totalItem: number = 0
+
+const fetchGoogleBookApi: any = async () => {
   const keywordElement = document.querySelector('input[name="keyword"]') as HTMLInputElement
   const { value } = keywordElement
-  const url: string = `https://www.googleapis.com/books/v1/volumes?q=intitle:${value}`
-  const apiResponse: any = axios
+  const params: string = value.replace(/[\u{20}\u{3000}]/u, '+')
+  let bookLists: any[] = []
+
+  const url: string = `https://www.googleapis.com/books/v1/volumes?q=intitle:${params}&maxResults=10&startIndex=0`
+  const apiResponse: any = await axios
     .get(url)
-    .then(response => console.log(response))
+    .then(response => response.data)
     .catch(error => window.alert(error))
+  totalItem = apiResponse.totalItems
+  
+  apiResponse.items.forEach((item: any) => {
+    let bookData: any = {
+      title: '',
+      subtitle: '',
+      publisheData: '',
+      description: '',
+      imageLinks: ''
+    }
+
+    bookData.title = item.title
+    bookData.subtitle = item.subtitle
+    bookData.publisheData = item.publisheData
+    bookData.description = item.description
+    bookData.imageLinks = item.imageLinks.smallThumbnail
+
+    bookLists.push(...bookData)
+  })
+
+  resultLists.push(bookLists)
 }
 
 const clickSearchButton = (): void => {
